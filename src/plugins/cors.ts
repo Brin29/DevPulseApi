33960913bcb5d083ab9f3af3a280d7a8fa0cb0b3
Brin29 <breinerstivenparracortes50@gmail.com>
2026-06-14 15:27:10 -0500

    add content security policy

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -1,9 +1,10 @@
 import fp from "fastify-plugin";
 import cors from "@fastify/cors";
+import { env } from "../env";
 
 export default fp(async (fastify) => {
   fastify.register(cors, {
-    origin: ["http://localhost:5173", "https://dev-pulse-front.vercel.app"],
+    origin: [env.FRONTEND_URL!!, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   });
